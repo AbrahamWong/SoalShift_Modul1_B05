d11899699332276df9d438f1cdb8f936101fdcbe
Fandy Kuncoro <fandy19061999@gmail.com>
2019-02-22 17:07:52 +0700

    edit path soal5

--- soal5.sh
+++ soal5.sh
@@ -3,4 +3,4 @@
 # Jika folder belum ada, maka dibuat terlebih dahulu
 mkdir -p /home/$USER/modul1
 
-awk '{if ($0 ~/cron/ && $0 !~ /sudo/ && NF < 13) print $0}' /var/log/syslog > record.log
+awk '{if ($0 ~/cron/ && $0 !~ /sudo/ && NF < 13) print $0}' /var/log/syslog > /home/$USER/modul1/record.log
