a87b2500a8042463f722fb22fd41de7c9e6abd2c
Fandy Kuncoro <fandy19061999@gmail.com>
2019-02-22 13:49:55 +0700

    jika folder belum ada, maka dibuat dulu

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-awk '{if ($0 ~/cron/ && $0 !~ /sudo/ && NF < 13) print $0}' /var/log/syslog > /home/$USER/modul1/record.log
+# Jika folder belum ada, maka dibuat terlebih dahulu
+mkdir -p /home/$USER/modul1
+
+awk '{if ($0 ~/cron/ && $0 !~ /sudo/ && NF < 13) print $0}' /var/log/syslog > record.log
